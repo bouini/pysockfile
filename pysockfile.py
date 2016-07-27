572e6859be80fc1795d91b10c4b5dde1179f6af2
bouini <bouini@users.noreply.github.com>
2016-07-27 22:03:54 +0200

    Add ability to transfer multiple files

--- pysockfile.py
+++ pysockfile.py
@@ -30,41 +30,42 @@ from contextlib import closing
 BUFFER_SIZE = 256 * 1024
 FBI_PORT = 5000
 
-def send_file(filename, ip):
-    filesize = os.stat(filename).st_size
-    nbfiles = struct.pack("!i", 1)
-    fbiinfo = struct.pack("!q", filesize)
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    with open(filename, "rb") as f:
-        with closing(socket.socket()) as sock:
-            sock.connect((ip, FBI_PORT))
-            sock.send(nbfiles)                
-            ack = sock.recv(1)
-            if ack == 0:
-                print "Send cancelled by remote"
-                return
-            sock.send(fbiinfo)                
-            loop = True
-            i = 0
-            nbchunks = filesize/BUFFER_SIZE;
-            if filesize%BUFFER_SIZE > 0:
-                nbchunks=nbchunks+1
-            while loop:
-                buf = f.read(BUFFER_SIZE)
-                if not buf:
-                    print "File \"" + filename + "\" sent successfully"
-                    loop = False
-                else:
-                    sock.send(buf)
-                    i=i+1
-                    print( "%d/%d"%(i,nbchunks) )
+def send_file(files, ip):
+    with closing(socket.create_connection((ip, FBI_PORT),2)) as sock:
+        sock.settimeout(None)
+        counter=len(files)
+        counterinfo=struct.pack('!i', counter)
+        sock.send(counterinfo)
+        print 'Sending files...'
+        for filename in files:
+            filesize=os.stat(filename).st_size
+            fbiinfo=struct.pack('!q', filesize)                      
+            with open(filename, 'rb') as f:
+                ack=sock.recv(1)
+                if ack == 0:
+                    print 'Send cancelled by remote'
+                    return
+                print 'Sending info for \'%s\'...'%filename
+                sock.send(fbiinfo)                
+                loop=True
+                i=0
+                print 'Sending data for \'%s\'...'%filename 
+                while loop:
+                    buf=f.read(BUFFER_SIZE)
+                    if not buf:
+                        print 'File \'%s\' sent successfully'%filename
+                        loop=False
+                    else:
+                        i+=sock.send(buf)
+                        print '%d/%d'%(i,filesize)
+        print 'All files sent successfully'
         
 def main(args):
     ip = args[1]
-    filename = args[2]
-    print "ip = " + ip
-    print "filename = " + filename
-    send_file(filename, ip)
+    files = args[2:]
+    print 'ip = ' + ip
+    print 'files = ' + str(files).strip('[]')
+    send_file(files, ip)
     return 0
 
 if __name__ == '__main__':
